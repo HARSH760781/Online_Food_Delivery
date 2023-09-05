8abb1b3c20220af125acfa0ba6cc0600d48d7d80
HARSH760781 <98482966+HARSH760781@users.noreply.github.com>
2023-09-05 21:31:33 +0530

    adding frontend folder

---
+++ frontend/src/App.js
@@ -0,0 +1,40 @@
+import "./App.css";
+import Header from "./component/Header";
+import { Outlet } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import { useEffect } from "react";
+import { setDataProduct } from "./redux/productSlide";
+import { useDispatch, useSelector } from "react-redux";
+
+function App() {
+  const dispatch = useDispatch();
+  const productData = useSelector((state) => state.product);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const res = await fetch(
+          `${process.env.REACT_APP_SERVER_DOMIN}/product`
+        );
+        const resData = await res.json();
+        dispatch(setDataProduct(resData));
+      } catch (error) {
+        console.log(`Failed to load the data from database:${error}`);
+      }
+    })();
+  });
+
+  return (
+    <>
+      <Toaster />
+      <div>
+        <Header />
+        <main className="pt-16 bg-slate-100 min-h-[calc(100vh)]">
+          <Outlet />
+        </main>
+      </div>
+    </>
+  );
+}
+
+export default App;
